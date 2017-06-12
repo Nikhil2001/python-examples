5c9f8a9e891541299e683764bc7c5015352011fe
Nikhil2001 <Nikhil2001@github.com>
2017-06-12 15:30:38 +0200

    new1

--- python_objects/arithemtic.py
+++ python_objects/arithemtic.py
@@ -3,3 +3,26 @@
 #add two numbers
 
 print 1+2
+
+# two subtract 
+
+print 5-4
+
+
+#two multiply 
+
+
+print 7*4
+
+#to divide 
+
+print 9/4
+
+print 9/4.0
+
+#check division 
+
+
+#power check
+
+print 3**2

1fc724ada7ef9281ecdcce908cb19d3a3f74390d
Nikhil2001 <Nikhil2001@github.com>
2017-06-12 15:49:52 +0200

    updated

--- python_objects/arithemtic.py
+++ python_objects/arithemtic.py
@@ -4,26 +4,27 @@
 #add two numbers
 
 print 1+2
-
+# 3
 # two subtract 
 
 print 5-4
-
+#1
 
 #two multiply 
 
 
 print 7*4
-
+#28
 #to divide 
 
 print 9/4
-
+#2
 print 9/4.0
-
+#2.25
 #check division.py file for true division and classic division
 
 
 # 3  power 2
 
 print 3**2
+#9
--- python_objects/cast.py
+++ python_objects/cast.py
@@ -3,5 +3,6 @@
 
 #casting  using functions 
 print float(3)/2
-
+#1.5
 print int(3.5)
+#3
